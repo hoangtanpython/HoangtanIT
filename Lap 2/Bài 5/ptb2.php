d5dd0b9d27a87f27f79041bf987dc8757f98bf0b
hoangtanpython <92588596+hoangtanpython@users.noreply.github.com>
2022-08-26 22:28:49 +0700

    ádfgh

--- Lap 2/Bài 5/ptb2.php
+++ Lap 2/Bài 5/ptb2.php
@@ -1,102 +1,75 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body>
-  <?php
-    // Pt bac nhat
-    function giaipt_bac_1($a, $b) {
-      if($a == 0) {
-        if($b == 0) {
-          $nghiem = "Phương trình vô số nghiệm";
-        }
-        if($b != 0) {
-          $nghiem = "Phương trình vô nghiệm";
-        } 
-      }
-      else {
-          $nghiem = "x=". round(-($b/$a),2);
-        }
-        return $nghiem;
-      
+<?php
+    $a = "";$b = "";$c = "";
+    $ketqua = "";
+    if(isset($_POST['a']) && isset($_POST['b']) && isset($_POST['c'])){
+        $a = $_POST['a'];$b = $_POST['b'];$c = $_POST['c'];
+        $ketqua = bac2($a,$b,$c);
     }
-    // Pt bac 2
-    function giaipt_bac_2($a, $b, $c) { 
-      if ($a==0) {
-        $nghiem = giaipt_bac_1($b, $c);  
-      }
-      if($a != 0) {
-        $delta = pow($b,2)-(4*$a*$c);
-     
-        if($delta == 0) {
-          $nghiem = "Phương trình có nghiệm kép x1=x2=".-($b/2*$a);
-        }
-        if($delta > 0) 
-        {
-    
-          $x1 = ((-$b+sqrt($delta))/2*$a);
-          $x2 = ((-$b-sqrt($delta))/2*$a);
-          $nghiem = "Phương trình có 2 nghiệm phân biệt x1 = $x1, x2 = $x2";
-        } else {
-          $nghiem = "Phương trình vô nghiệm";
+    function bac2($a , $b , $c){
+        if($a == 0){
+            if($b == 0){
+                if($c == 0){
+                    return "Vô số nghiệm";
+                }else{
+                    return "Vô nghiệm";
+                }
+            }else{
+                return "X = ". -$c/$b;
+            }
+        }else{
+            $delta = $b*$b - 4*$a*$c;
+            if($delta < 0){
+                return "Vô nghiệm";
+            }else if($delta == 0){
+                return "X = ". -$b/(2*$a);
+            }else{
+                return "X1 = ". round((-$b - sqrt($delta))/(2*$a),2) ." X2 = ". round((-$b + sqrt($delta))/(2*$a),2);
+            }
         }
     }
-    return $nghiem;
-  }
-  if(isset($_POST["a"]) && isset($_POST["b"]) && isset($_POST["c"])) {
-    $nghiem = giaipt_bac_2($a = $_POST['a'], $b = $_POST['b'], $c = $_POST['c']);
-  }
-    ?>
-    <form action="Bai_5.php" method="post">
-      <table width="806" border="1">
-        <tr>
-          <td colspan="4" bgcolor="#336699">
-            <strong>Giải phương trình bậc 2</strong>
-          </td>
-        </tr>
-        <tr>
-          <td width="83">Phương trình</td>
-          <td width="236">
-            <input name="a" type="text" value="<?php echo $a; ?>">
-            X^2 +
-          </td>
-          <td width="218">
-            <label for="textfield3"></label>
-            <input type="text" name="b" id="textfield3" value="<?php echo $b; ?>">
-            X+
-          </td>
-          <td width="241">
-            <label for="textfield"></label>
-            <input type="text" name="c" id="textfield" value="<?php echo $c; ?>">
-            =0
-          </td>
-        </tr>
-        <tr>
-          <td colspan="4">
-            Nghiệm
-            <label for="textfield2"></label>
-            <input
-              name="textfield"
-              type="text"
-              id="textfield2"
-              width="400"
-              value="<?php if (isset($nghiem)) {
-                echo $nghiem;
-              } ?>"
-            />
-          </td>
-        </tr>
+?>
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
-        <tr>
-          <td colspan="4" align="center" valign="middle">
-            <input type="submit" name="chao" id="chao" value="Xuất" />
-          </td>
-        </tr>
-      </table>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Untitled Document</title>
+</head>
+
+<body>
+    <form action="giai_pt_bac_2.php" method="post">
+        <table width="806" border="1">
+            <tr>
+                <td colspan="4" bgcolor="#336699"><strong>Giải phương trình bậc 2</strong></td>
+            </tr>
+            <tr>
+                <td width="83">Phương trình </td>
+                <td width="236">
+                    <input name="a" type="text" value="<?=htmlspecialchars($a);?>"/>
+                    X^2 +
+                </td>
+                <td width="218"><label for="textfield3"></label>
+                    <input type="text" name="b" id="textfield3" value="<?=htmlspecialchars($b);?>" />
+                    X+
+                </td>
+                <td width="241"><label for="textfield"></label>
+                    <input type="text" name="c" id="textfield" value="<?=htmlspecialchars($c);?>"/>
+                    =0
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4">
+                    Nghiệm
+                    <label for="textfield2"></label>
+                    <input name="textfield" type="text" id="textfield2" width="400" value="<?=htmlspecialchars($ketqua);?>" />
+            </tr>
+            <tr>
+                <td colspan="4" align="center" valign="middle"><input type="submit" name="chao" id="chao"
+                        value="Xuất" /></td>
+            </tr>
+        </table>
     </form>
-  </body>
+</body>
+
 </html>
